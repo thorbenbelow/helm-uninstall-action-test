52fe3b785bed138674a1286ad9fcf8d270f52f17
Thorben Below <56894536+thorbenbelow@users.noreply.github.com>
2024-02-06 21:23:22 +0100

    use default helm chart
    Signed-off-by: Thorben Below <56894536+thorbenbelow@users.noreply.github.com>

--- main.go
+++ main.go
@@ -8,24 +8,29 @@ import (
 	"os"
 )
 
+var settings = cli.New()
+
 func debug(format string, v ...interface{}) {
-	format = fmt.Sprintf("[debug] %s\n", format)
-	log.Output(2, fmt.Sprintf(format, v...))
+	if settings.Debug {
+		format = fmt.Sprintf("[debug] %s\n", format)
+		log.Output(2, fmt.Sprintf(format, v...))
+	}
 }
 
 func main() {
-	settings := cli.New()
 	cfg := new(action.Configuration)
-	//settings.SetNamespace("helm-test")
 
+	// Create a new uninstall action
 	un := action.NewUninstall(cfg)
+
+	// Initialize the config with the release namespace
 	if err := cfg.Init(settings.RESTClientGetter(), "helm-test", os.Getenv("HELM_DRIVER"), debug); err != nil {
 		log.Fatal(err)
 	}
 
+	// Uninstall the release
 	_, err := un.Run("test")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
